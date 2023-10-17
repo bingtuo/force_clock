7cc9592758ca06a913261f159108f675fcdd6d55
冰拓 <38130106+bingtuo@users.noreply.github.com>
2023-10-17 16:19:25 +0800

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,13 @@
+import time
+
+minutes = int(input("输入你要专注多少分钟："))
+seconds = minutes * 60
+
+while seconds:
+    min, secs = divmod(seconds, 60)
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    print(timer, end="\r")
+    time.sleep(1)
+    seconds -= 1    
+
+print("时间到了！")
